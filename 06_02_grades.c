6138bfba740e22f2b72524662a30f22cc411acf2
Zach Williams <hey@zachwill.com>
2011-09-11 01:24:40 -0700

    Add grade exercise

---
+++ 06_02_grades.c
@@ -0,0 +1,33 @@
+// Excercise 6.2
+// =============
+//
+// Given a numeric grade, print the letter.
+
+#include <stdio.h>
+
+int grade;
+
+int main(int argc, const char *argv[]) {
+    if (argc == 2) {
+        grade = atoi(argv[1]);
+    } else {
+        printf("You didn't pass a grade argument.");
+        return 0;
+    }
+
+    if (grade == 100) {
+        printf("F");
+    } else if (grade >= 90) {
+        printf("A");
+    } else if (grade >= 80) {
+        printf("B");
+    } else if (grade >= 70) {
+        printf("C");
+    } else if (grade >= 60) {
+        printf("D");
+    } else {
+        printf("F");
+    }
+
+    return 0;
+}
