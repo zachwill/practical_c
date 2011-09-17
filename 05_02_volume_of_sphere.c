32f4d452885b41dd18445a68c65284c9f13d92d9
Zach Williams <hey@zachwill.com>
2011-09-17 00:05:48 -0700

    Add sphere radius to volume exercise

---
+++ 05_02_volume_of_sphere.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  05_02_volume_of_sphere.c
+ *
+ *    Description:  Exercise 5.2
+ *                  Write a program to calculate the volume of a sphere.
+ *
+ *        Version:  1.0
+ *        Created:  09/16/2011 23:54:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zach Williams, hey AT zachwill DOT com
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+float sphere_radius_to_volume(int radius){
+    float float_radius, conversion = (4.0/3.0), pi = 3.14;
+    float_radius = (float) radius;
+    return conversion * pi * pow(float_radius, 3);
+}
+
+int main(int argc, const char *argv[]) {
+    if (argc == 1) {
+        printf("You didn't include the radius of a sphere.");
+        return 1;
+    }
+    int radius;
+    float volume;
+    radius = atoi(argv[1]);
+    volume = sphere_radius_to_volume(radius);
+    printf("%f\n", volume);
+    return 0;
+}
