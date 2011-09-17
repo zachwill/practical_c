66af3625376b5681a08571530b3a89d8c8aaa1d4
Zach Williams <hey@zachwill.com>
2011-09-17 00:41:31 -0700

    Add hour and minute conversion exercise

---
+++ 05_05_hour_and_minutes.c
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  05_05_hour_and_minutes.c
+ *
+ *    Description:  Exercise 5.5
+ *                  Write a program that takes hour and minutes as input, and
+ *                  then outputs the total number of minutes.
+ *
+ *        Version:  1.0
+ *        Created:  09/17/2011
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zach Williams, hey AT zachwill DOT com
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int hour, minutes, total_minutes;
+
+int calculate_total_minutes(int hour, int minutes) {
+    int total;
+    total = hour * 60 + minutes;
+    return total;
+}
+
+int main(int argc, const char *argv[]) {
+    if (argc == 1) {
+        printf("You didn't enter hour or minute arguments.");
+        return 1;
+    } else if(argc == 2) {
+        hour = atoi(argv[1]);
+        total_minutes = hour * 60;
+    } else {
+        hour = atoi(argv[1]);
+        minutes = atoi(argv[2]);
+        total_minutes = calculate_total_minutes(hour, minutes);
+    }
+    printf("%d total minutes\n", total_minutes);
+    return 0;
+}
